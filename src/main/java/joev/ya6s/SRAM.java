392bd5ed30d69722f54eee230f665f280843622f
Joseph Vigneau <joev@alum.wpi.edu>
2021-02-25 19:42:37 -0500

    feat: Use a base and mask for SRAM selection.

--- src/main/java/joev/ya6s/SRAM.java
+++ src/main/java/joev/ya6s/SRAM.java
@@ -12,22 +12,33 @@ public class SRAM {
   private final Signal rwb;
   private final Signal clock;
   private final Signal.Listener tickFn;
+  private final short maskedAddress;
+  private final short mask;
   private final byte[] memory = new byte[0x10000];
+
+  /**
+   * Create a Static RAM module that takes up the entire address space.
+   *
+   * @param backplane the backplane to attach to.
+   */
+  public SRAM(Backplane backplane) {
+    this(backplane, (short)0, (short)0);
+  }
 
   /**
    * Create a Static RAM module with the given busses and signals.
    *
-   * @param address A 16-bit address bus.
-   * @param data An 8-bit data bus.
-   * @param rwb A signal that determines if data should be written to the bus (true) or read from it (false)
-   * @param clock The clock signal, read or write memory upon a negative clock transition.
+   * @param backplane the backplane to attach to.
+   * @param baseAddress the base address of the SRAM.
+   * @param mask the address mask of the SRAM.
    */
-  public SRAM(Backplane backplane) {
-    //Bus address, Bus data, Signal rwb, Signal clock) {
+  public SRAM(Backplane backplane, short baseAddress, short mask) {
     address = backplane.address();
     data = backplane.data();
     rwb = backplane.rwb();
     clock = backplane.clock();
+    this.mask = mask;
+    this.maskedAddress = (short)(baseAddress & mask);
 
     tickFn = this::tick;
     clock.register(tickFn);
@@ -40,12 +51,14 @@ public class SRAM {
    */
   private void tick(Signal.EventType eventType) {
     if(eventType == Signal.EventType.POSITIVE_EDGE) {
-      // check address.value to see if we're selected.
-      if(rwb.value()) {
-        data.value(memory[address.value()]);
-      }
-      else {
-        memory[address.value()] = (byte)data.value();
+      short busAddr = (short)address.value();
+      if((short)(busAddr & mask) == maskedAddress) {
+        if(rwb.value()) {
+          data.value(memory[busAddr & 0xFFFF]);
+        }
+        else {
+          memory[busAddr & 0xFFFF] = (byte)data.value();
+        }
       }
     }
   }
